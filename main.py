28cd76e9d2ee187974a56ede817144445ba5107f
Ivan Zyulin <solar.ivz@gmail.com>
2024-04-30 01:29:02 +1200

    add functions load, save obj

--- main.py
+++ main.py
@@ -25,7 +25,7 @@
 как сохранение информации о зоопарке в файл и возможность её загрузки,
 чтобы у вашего зоопарка было "постоянное состояние" между запусками
 программы.'''
-
+import pickle
 class Animal():
     def __init__(self, name, age):
         self.name = name
@@ -105,6 +105,24 @@ def animal_sound(animals):
     for animal in animals:
         animal.make_sound()
 
+# Функция для сохранения объекта зоопарка в файл
+def save_zoo(zoo):
+    filename = 'zoo_data.pkl'
+    with open(filename, 'wb') as file:
+        pickle.dump(zoo, file)
+    print(f"Объекты зоопарка сохранены в файл {filename}.")
+
+# Функция для загрузки объекта зоопарка из файла
+def load_zoo():
+    try:
+        with open('zoo_data.pkl', 'rb') as file:
+            zoo = pickle.load(file)
+            print("Объекты зоопарка загружены из файла.")
+            return zoo
+    except FileNotFoundError:
+        print("Файл с данными зоопарка не найден.")
+        return None
+
 # Пример использования функции animal_sound с использованием списка обьектов животных:
 animals = [Bird("Воробей", 2), Mammal("Собака", 3), Reptile("Змея", 1)]
 animal_sound(animals)
@@ -135,3 +153,27 @@ zoo.view_staffs()
 zoo.staffs[0].feed_animal(animals[0])
 zoo.staffs[2].heal_animal(animals[2])
 
+# Основной цикл программы
+while True:
+    print("\nВыберите действие:")
+    print("1. Просмотреть животных в зоопарке")
+    print("2. Просмотреть сотрудников зоопарка")
+    print("3. Сохранить объекты зоопарка в файл")
+    print("4. Загрузить объекты зоопарка из файла")
+    print("5. Выйти из программы")
+
+    choice = input("Введите номер действия: ")
+
+    if choice == '1':
+        zoo.view_animals()
+    elif choice == '2':
+        zoo.view_staffs()
+    elif choice == '3':
+        save_zoo(zoo)
+    elif choice == '4':
+        zoo = load_zoo()
+    elif choice == '5':
+        print("Выход из программы.")
+        break
+    else:
+        print("Некорректный выбор. Пожалуйста, введите номер от 1 до 5.")
